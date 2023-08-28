9fe95fe7f89333324de92e5a1cd0585562711c41
Matheus Padilha <mvpadilha94@gmail.com>
2023-08-27 22:38:17 -0300

    Examples with functions

--- src/main/kotlin/br/com/matheuspadilha/bytebank/Main.kt
+++ src/main/kotlin/br/com/matheuspadilha/bytebank/Main.kt
@@ -1,11 +1,47 @@
 package br.com.matheuspadilha.bytebank
 
-import br.com.matheuspadilha.bytebank.modelo.Endereco
-
 fun main(args: Array<String>) {
-
+    testTipoFuncaoReferencia()
+    testTipoFuncaoClass()
+    testTipoFuncaoLambda()
+    testTipoFuncaoAnonima()
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
+}
+
+fun testTipoFuncaoLambda() {
+    val minhaFuncaoLambda: () -> Unit = {
+        println("Executa como Lambda")
+    }
+    println(minhaFuncaoLambda())
+}
+
+fun testTipoFuncaoAnonima() {
+    val minhaFuncaoAnonima: () -> Unit = fun() {
+        println("Executa como Anonima")
+    }
+    println(minhaFuncaoAnonima())
+}
+
+fun testTipoFuncaoClass() {
+    val minhaFuncaoClass: () -> Unit = Test()
+    println(minhaFuncaoClass())
+}
+
+fun testTipoFuncaoReferencia() {
+    val minhaFuncao: () -> Unit = ::test
+    println(minhaFuncao()) // Seguira o padrão lazzy so ira executar quando for obrigado a executar
+}
+
+fun test() {
+    println("Executa teste")
+}
+
+class Test : () -> Unit {
+    override fun invoke() {
+        println("Executa invoke do Test")
+    }
+
 }
